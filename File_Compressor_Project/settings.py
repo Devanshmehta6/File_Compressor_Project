fbd0f3caad78c82238bc88b792b0ec6031b3c724
devanshmehtaGitHub <devanshgithub@gmail.com>
2024-11-01 17:07:39 +0530

    allowed hosts

--- File_Compressor_Project/settings.py
+++ File_Compressor_Project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-52%0zht*^7!!z-tvuhu&b1(is793o26*vlsmu7jpxtkh6ixoh4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", 'fileCompressor.pythonanywhere.com']
 
 
 # Application definition
